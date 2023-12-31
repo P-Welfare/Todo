2a78ddfe16fad471767e6c78596dc35e2a9de0de
P-Welfare <pwelfare@gmail.com>
2023-12-31 21:47:52 +0700

    Added basic functions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,58 @@
-class Todo {
+const { isToday, isTomorrow } = require("date-fns")
+
+projectList = [] 
+
+class toDo {
     constructor(title, description, dueDate, priority,) {
     this.title = title
     this.description = description
     this.dueDate = dueDate
     this.priority = priority
     }   
+
+
 }
+// construct project
+const project = (title) => {
+    const tasks = []
+    return {title, tasks}
+};
+// create test project
+defaultProject = project('Today')
+
+// basic add task function
+function addTask(task) {
+    defaultProject.tasks.push(task)
+};
+
+// default todo test
+defaultToDo = new toDo('brush teeth', 'brush em good', 'today', 'high')
+
+
+// create test todo add
+addTask(defaultToDo)
+
+function addProject(newProject) {
+    projectList.push(newProject)
+}
+
+addProject(defaultProject)
 
 /*
 default array of todos 
 CRUD 
 option to change array (project) todo
+
+
 add new array of todos 
 CRUD 
 option to change array (project) of todo
 
 
-
+SEPERATE THE APPLICATION LOGIC =============== FROM THE MODULES 
 
 */
+
+
+
+
